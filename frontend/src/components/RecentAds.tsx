de56769ee4ca0d6c302164518a15015a55748981
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2023-10-11 21:28:27 +0200

    style: update footer color

--- frontend/src/components/RecentAds.tsx
+++ frontend/src/components/RecentAds.tsx
@@ -16,7 +16,7 @@ const RecentAds = () => {
   }, []);
 
   const [total, setTotal] = useState(0);
-  
+
   // useEffect(() => {
   //   const fetchData = async () => {
   //     try {
@@ -32,17 +32,13 @@ const RecentAds = () => {
   return (
     <div className="pt-6">
       <h2 className="text-2xl mb-6">Annonces récentes</h2>
-      <section className= "flex flex-wrap pb-24">
+      <section className="flex flex-wrap pb-24">
         {ads.map((ad) => (
-          <AdCard
-            key={ad.title}
-            ad={ad}
-            link={`/ads/${ad.id}`}
-          />
+          <AdCard key={ad.id} ad={ad} link={`/ads/${ad.id}`} />
         ))}
       </section>
     </div>
-  )
-}
+  );
+};
 
 export default RecentAds;
